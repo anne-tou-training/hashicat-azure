7a00d933318b4028fc6b7d6934636fcce6ba55e7
Anne Tou <annetoutest@gmail.com>
2021-02-03 19:47:18 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/AnneTou-Training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "azurerm_resource_group"
+  resource_group_name = "${var.prefix}-workshop"
 }
